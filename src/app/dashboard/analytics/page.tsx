86143d2dbff1e90b0e067519264a258f1bf3efde
vamik <vamiksiddiqui666@gmail.com>
2024-11-11 16:23:44 +0530

    "

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { useEffect, useState } from "react";
 import { user_data, graph_data } from "./user_data";
@@ -12,7 +13,7 @@ export default function Page() {
   const [users, setUsers] = useState(user_data);
   const [searchInput, setSearchInput] = useState("");
 
-  const handleChange = (e) => {
+  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setSearchInput(e.target.value);
   };
 
@@ -60,7 +61,7 @@ export default function Page() {
       tooltip: {
         enabled: true,
         callbacks: {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        
           label: function (context: { raw: any; }) {
             return context.raw;
           }

78872f60b65d6253093770354d8af353f3f326d6
vamik <vamiksiddiqui666@gmail.com>
2024-11-12 15:34:37 +0530

    changes to cors

--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -4,28 +4,31 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const page = searchParams.get('page') || 1;  // Default to page 1 if no query param
 
+  console.log('Fetching data for page:', page); // Debug log
+
   try {
     const response = await axios.get('https://newsapi.org/v2/top-headlines', {
       params: {
         country: 'us',
         language: 'en',
-        apiKey: process.env.NEWS_API_KEY,
+        apiKey: process.env.NEXT_NEWS_API_KEY, 
         page,
         pageSize: 6,
       },
     });
 
- 
+    console.log('News API response:', response.data); // Debug log
+
     return new Response(JSON.stringify(response.data), {
       status: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': '*',  // CORS
         'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',  
+        'Access-Control-Allow-Headers': 'Content-Type',
       },
     });
   } catch (error) {
-    console.error('Error fetching data from News API:', error);
+    console.error('Error fetching data from News API:', error.response?.data || error.message || error);
     return new Response('Error fetching data from News API', { status: 500 });
   }
 }
